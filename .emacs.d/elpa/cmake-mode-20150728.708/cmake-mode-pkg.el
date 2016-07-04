413e7af823bbae5c8d8ab3eab6049f67a99776fc
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2016-07-04 12:27:40 +0200

    Update elpa

--- .emacs.d/elpa/cmake-mode-20150728.708/cmake-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "cmake-mode" "20150728.708" "No description available." 'nil)
--- .emacs.d/elpa/imenu-anywhere-20151030.1127/imenu-anywhere-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "imenu-anywhere" "20151030.1127" "ido/helm imenu tag selection across all buffers with the same mode" (quote ((cl-lib "0.5"))))
