3048f4106300a6b4f93eb051e70bfe6f3b84bb4f
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2017-06-05 14:31:43 +0200

    .emacs: Update elpa

---
+++ .emacs.d/elpa/helm-gitlab-0.8.0/helm-gitlab-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-gitlab" "0.8.0" "Helm interface to Gitlab" '((s "1.9.0") (dash "2.9.0") (helm "1.0") (gitlab "0.8.0")) :url "https://github.com/nlamirault/emacs-gitlab" :keywords '("gitlab" "helm"))
---
+++ .emacs.d/elpa/plantuml-mode-1.2.3/plantuml-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "plantuml-mode" "1.2.3" "Major mode for PlantUML" '((emacs "24")) :keywords '("uml" "plantuml" "ascii"))
