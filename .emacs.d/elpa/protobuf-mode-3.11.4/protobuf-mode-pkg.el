400ad3eec1cdb4375b7f12d4708cc282a33500fd
Witaut Bajaryn <vitaut.bayaryn@gmail.com>
2020-03-19 11:52:13 +0100

    .emacs.d: Add protobuf-mode and lua-mode

---
+++ .emacs.d/elpa/protobuf-mode-3.11.4/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "3.11.4" "major mode for editing protocol buffers." 'nil :commit "d0bfd5221182da1a7cc280f3337b5e41a89539cf" :keywords '("google" "protobuf" "languages") :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com"))
