d871a5072a77da7c1bec197812a1fca2f928349e
Witaut Bajaryn <vitaut.bayaryn@gmail.com>
2022-06-16 11:53:23 +0200

    .emacs.d: Update elpa

--- .emacs.d/elpa/forth-mode-20170527.1930/forth-mode-autoloads.el
+++ .emacs.d/elpa/forth-mode-20170527.1930/forth-mode-autoloads.el
@@ -6,22 +6,34 @@
                          (or (file-name-directory #$) (car load-path))))
 
 
-;;;### (autoloads nil "forth-block-mode" "forth-block-mode.el" (0
-;;;;;;  0 0 0))
-;;; Generated autoloads from forth-block-mode.el
+;;;### (autoloads nil "forth-block-mode" "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-block-mode.el"
+;;;;;;  "76e536523a97116f6561ed7cb9d22b22")
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-block-mode.el
 
 (autoload 'forth-block-mode "forth-block-mode" "\
 Minor mode for Forth code in blocks.
 
+If called interactively, enable Forth-Block mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "forth-block-mode"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-block-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-block-mode.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "forth-block-mode" '("forth-")))
 
 ;;;***
+
+;;;***
 
-;;;### (autoloads nil "forth-interaction-mode" "forth-interaction-mode.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from forth-interaction-mode.el
+;;;### (autoloads nil "forth-interaction-mode" "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-interaction-mode.el"
+;;;;;;  "ef3e150ad93b981f466d8f8bbd29e314")
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-interaction-mode.el
 
 (autoload 'forth-kill "forth-interaction-mode" "\
 
@@ -29,24 +41,16 @@ Minor mode for Forth code in blocks.
 \(fn &optional BUFFER)" t nil)
 
 (autoload 'run-forth "forth-interaction-mode" "\
-Start an interactive forth session.
+Start an interactive forth session." t nil)
 
-\(fn)" t nil)
-
-(autoload 'forth-restart "forth-interaction-mode" "\
-
-
-\(fn)" t nil)
+(autoload 'forth-restart "forth-interaction-mode" nil t nil)
 
 (autoload 'forth-interaction-send "forth-interaction-mode" "\
 
 
 \(fn &rest STRINGS)" nil nil)
 
-(autoload 'forth-words "forth-interaction-mode" "\
-
-
-\(fn)" nil nil)
+(autoload 'forth-words "forth-interaction-mode" nil nil nil)
 
 (autoload 'forth-eval "forth-interaction-mode" "\
 
@@ -58,10 +62,7 @@ Start an interactive forth session.
 
 \(fn START END)" t nil)
 
-(autoload 'forth-eval-defun "forth-interaction-mode" "\
-
-
-\(fn)" t nil)
+(autoload 'forth-eval-defun "forth-interaction-mode" nil t nil)
 
 (autoload 'forth-load-file "forth-interaction-mode" "\
 
@@ -73,32 +74,28 @@ Start an interactive forth session.
 
 \(fn WORD)" t nil)
 
-(autoload 'forth-switch-to-output-buffer "forth-interaction-mode" "\
+(autoload 'forth-switch-to-output-buffer "forth-interaction-mode" nil t nil)
 
+(autoload 'forth-switch-to-source-buffer "forth-interaction-mode" nil t nil)
 
-\(fn)" t nil)
+(autoload 'forth-eval-last-expression "forth-interaction-mode" nil t nil)
 
-(autoload 'forth-switch-to-source-buffer "forth-interaction-mode" "\
-
-
-\(fn)" t nil)
-
-(autoload 'forth-eval-last-expression "forth-interaction-mode" "\
-
-
-\(fn)" t nil)
-
-(autoload 'forth-eval-last-expression-display-output "forth-interaction-mode" "\
-
-
-\(fn)" t nil)
+(autoload 'forth-eval-last-expression-display-output "forth-interaction-mode" nil t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "forth-interaction-mode"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-interaction-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-interaction-mode.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "forth-interaction-mode" '("forth-")))
 
 ;;;***
+
+;;;***
 
-;;;### (autoloads nil "forth-mode" "forth-mode.el" (0 0 0 0))
-;;; Generated autoloads from forth-mode.el
+;;;### (autoloads nil "forth-mode" "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-mode.el"
+;;;;;;  "4445c6ecc6a6079c942915a0dd2d3bbd")
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-mode.el
 
 (autoload 'forth-mode "forth-mode" "\
 Major mode for editing Forth files.
@@ -106,40 +103,61 @@ Major mode for editing Forth files.
 \(fn)" t nil)
 
 (add-to-list 'auto-mode-alist '("\\.\\(f\\|fs\\|fth\\|4th\\)\\'" . forth-mode))
+
+;;;### (autoloads "actual autoloads are elsewhere" "forth-mode" "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-mode.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "forth-mode" '("forth-")))
 
 ;;;***
+
+;;;***
 
-;;;### (autoloads nil "forth-parse" "forth-parse.el" (0 0 0 0))
-;;; Generated autoloads from forth-parse.el
+;;;### (autoloads "actual autoloads are elsewhere" "forth-parse"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-parse.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-parse.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "forth-parse" '("forth-")))
 
 ;;;***
 
-;;;### (autoloads nil "forth-smie" "forth-smie.el" (0 0 0 0))
-;;; Generated autoloads from forth-smie.el
+;;;### (autoloads "actual autoloads are elsewhere" "forth-smie" "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-smie.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-smie.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "forth-smie" '("forth-smie-")))
 
 ;;;***
 
-;;;### (autoloads nil "forth-spec" "forth-spec.el" (0 0 0 0))
-;;; Generated autoloads from forth-spec.el
+;;;### (autoloads "actual autoloads are elsewhere" "forth-spec" "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-spec.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-spec.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "forth-spec" '("forth-spec-")))
 
 ;;;***
 
-;;;### (autoloads nil "forth-syntax" "forth-syntax.el" (0 0 0 0))
-;;; Generated autoloads from forth-syntax.el
+;;;### (autoloads "actual autoloads are elsewhere" "forth-syntax"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-syntax.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-syntax.el
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "forth-syntax" '("forth-syntax-")))
 
 ;;;***
 
-;;;### (autoloads nil nil ("forth-mode-pkg.el") (0 0 0 0))
+;;;### (autoloads nil nil ("../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-block-mode.el"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-interaction-mode.el"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-mode-autoloads.el"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-mode-pkg.el"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-mode.el"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-parse.el"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-smie.el"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-spec.el"
+;;;;;;  "../../../../../../home/vozhyk/.emacs.d/elpa/forth-mode-20170527.1930/forth-syntax.el")
+;;;;;;  (0 0 0 0))
 
 ;;;***
 
